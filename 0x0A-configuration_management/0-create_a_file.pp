eaf9282b6ec469ac8b33caf9174e934eeb49b9e2
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-03-22 11:03:29 -0700

    Creating File using puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # This file uses puppet to create a file in /tmp
 
-file { 'resource title':
-  path       => /tmp/school
-  permission => 0744
-  owner      => www-data
-  group      => www-data
-  content    => I love Puppet
+file { '/tmp/school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => "I love Puppet",
 }
