ec2384b1a3caa65377ae811c1f4e86318ecac4ec
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-03-22 12:40:02 -0700

    method 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,14 @@
-# Pip installing flask version 2.1.0 using puppet
+# Pip installing flask version 2.1.0
+# and werkzeug version 2.1.1 using puppet
 
-pip3 {'flask':
-  ensure   => '2.1.0'
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
+
+# Installs Werkzeug
+package {'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
+}
+
