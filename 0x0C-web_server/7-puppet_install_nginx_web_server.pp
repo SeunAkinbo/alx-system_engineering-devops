9de2a08c890a74f76adbc330a937de90120e7c34
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-03-26 15:56:28 -0700

    Updating Puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,18 +1,20 @@
 # Puppet script to configure Nginx server
 
-#include stdlib
 
+# Install nginx package
 package { 'nginx':
   ensure  => installed,
 }
 
-# Installing nginx
-exec { 'install nginx':
-  command  => 'sudo apt -y update && sudo apt -y install nginx',
-  provider => shell,
-}
+# Installing nginx package
+#exec { 'install nginx':
+#  command  => 'sudo apt -y update && sudo apt -y install nginx',
+#  provider => shell,
+#}
 
+#Define a custom site configuration for nginx
 file { '/etc/nginx/sites-available/default':
+  ensure  => present,
   content => "server {
 		listen 80 default_server;
 		server_name _;
@@ -22,15 +24,18 @@ file { '/etc/nginx/sites-available/default':
           	}
 		rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
 	}",
-  require => Exec['install nginx'],
+  notify  => Service['nginx'],
+  require => Package['nginx'],
 }
 
+#Define the index.html file content
 file { '/var/www/html/index.html':
   ensure  =>  'file',
   content =>  'Hello World!',
-  require =>  Exec['install nginx'],
+  require =>  Package['nginx'],
 }
 
+# Ensure nginx is running
 exec { 'run':
   command  => 'sudo service nginx restart',
   provider => shell,
